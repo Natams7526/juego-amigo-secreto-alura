a876ad6d88452c726f7d66d576fd7d3a46445c61
Natams7526 <developmentnm2022@gmail.com>
2025-01-27 15:44:23 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 let amigos = [];
 
+// funcion encargada 
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     // invocar funcion encargada de validar la entrada 
     validarEntrada(nombreAmigo);
     // vaciar el input 
     document.getElementById('amigo').value = '';
+    actualizarLista();
     console.log(nombreAmigo);
     console.log(amigos);
 
 }
 
+
+// funcion que valida que el input no este vacio y empujar al array el nombre 
 function validarEntrada(nombre) {
     // Verificar si nombre es null, y quitamos los espacios en blanco con.trim  para verificar si es una cadena vacía
     if (!nombre || nombre.trim() === '') {
@@ -18,4 +22,18 @@ function validarEntrada(nombre) {
     } else {
         amigos.push(nombre);
     }
+}
+
+ // Función para recorrer el array amigos y mostrarlo en la lista HTML
+ function actualizarLista() {
+
+    const lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML = '';
+    
+    // Recorrer el array con un for y  Agregar cada amigo como <li>
+    for (let i = 0; i < amigos.length; i++) {        
+        lista.innerHTML += `<li>${amigos[i]}</li>`; 
+    }
+
+    
 }
