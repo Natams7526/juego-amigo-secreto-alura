5fe51cc455d3e2f241a143b066518ec01f3aadac
Natams7526 <developmentnm2022@gmail.com>
2025-01-27 14:58:45 -0500

    Implementar una función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 let amigos = [];
 
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    // invocar funcion encargada de validar la entrada 
+    validarEntrada(nombreAmigo);
+    // vaciar el input 
+    document.getElementById('amigo').value = '';
+    console.log(nombreAmigo);
+    console.log(amigos);
+
+}
+
+function validarEntrada(nombre) {
+    // Verificar si nombre es null, y quitamos los espacios en blanco con.trim  para verificar si es una cadena vacía
+    if (!nombre || nombre.trim() === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(nombre);
+    }
+}
